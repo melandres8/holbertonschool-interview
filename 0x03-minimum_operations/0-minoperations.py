70babee498f1433777aa89fc5c95ef057f70a5c5
Melkin Mosquera <melanmosta@gmail.com>
2021-02-18 12:36:41 -0500

    minoperations

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -7,12 +7,12 @@ def minOperations(n):
     needed to result in exactly n H characters in the file."""
     if n <= 1 or type(n) != int:
         return 0
-    else:
-        nums = []
-        for i in range(2, n + 1):
-            if n % i == 0:
-                nums.append(i)
-                n = n / i
-            else:
-                i += 1
-        return sum(nums)
+
+    nums = []
+    for i in range(3, n + 1):
+        if n % i == 0:
+            nums.append(i)
+            n = n / i
+        else:
+            i += 1
+    return sum(nums)
